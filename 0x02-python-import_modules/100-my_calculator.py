9147349e5c771f585b24ef4c7c8d06749f709fea
franklinniy <franklinniy@gmail.com>
2024-09-19 02:04:39 +0300

    build

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 import sys
+from calculator_1 import add, sub, mul, div
 
-def main():
-    if len(sys.argv) != 4:
+
+def my_calc():
+    args = sys.argv[1:]
+    length = len(args)
+    if (length != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
-    try:
-        a = int(sys.argv[1])
-        operator = sys.argv[2]
-        b = int(sys.argv[3])
-    except ValueError:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        if b == 0:
-            print("Error: Division by zero")
-            exit(1)
-        result = div(a, b)
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        a = int(args[0])
+        b = int(args[2])
+        if args[1] == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif args[1] == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif args[1] == "/*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif args[1] == "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
 
-    print(f"{a} {operator} {b} = {result}")
 
 if __name__ == "__main__":
-    main()
+    my_calc()

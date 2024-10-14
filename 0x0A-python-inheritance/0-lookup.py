e10ff29746b4bf2bf38a5bba1ad3fa2226614fab
franklinniy <franklinniy@gmail.com>
2024-10-15 01:10:54 +0300

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+checks for the attributes and methods of an object
+"""
+def lookup(obj):
+    """
+    lists the attributes and methods
+    Return: list of attributes and methods
+    """
+    return dir(obj)

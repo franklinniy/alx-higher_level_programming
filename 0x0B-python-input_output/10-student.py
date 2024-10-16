7fcac76a806896862efd0380371199b82df6d945
franklinniy <franklinniy@gmail.com>
2024-10-17 00:26:04 +0300

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+defines a student
+"""
+
+
+class Student():
+    """
+    student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialisation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        converts class obj to json
+        """
+        if attrs is not None and isinstance(attrs, list):
+            return {key: getattr(self, key) for key in
+                    attrs if isinstance(key, str) and hasattr(self, key)}
+        else:
+            return self.__dict__

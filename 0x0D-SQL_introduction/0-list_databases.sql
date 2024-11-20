1eb7172c1c6fa2f4711742d9336448c2ed4b45b2
franklinniy <franklinniy@gmail.com>
2024-11-20 17:00:08 +0300

    list databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-# MySQL username and password
-MYSQL_USER="your_username"
-MYSQL_PASSWORD="your_password"
-
-# MySQL command to list databases
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;"
+--Lists all databases in the server
+SHOW DATABASES;
 
